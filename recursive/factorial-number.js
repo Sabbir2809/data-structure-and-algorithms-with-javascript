1fcc991da403dcd5bba709f334a0dbc48239e3d5
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-02-09 19:04:34 +0600

    Recursive way to solve factorial and fibonacci

--- recursive/factorial-number.js
+++ recursive/factorial-number.js
@@ -5,15 +5,15 @@
   positive integers less than or equal to n. Factorial of zero is 1.
 */
 
-function factorial(n) {
+function recursiveFactorial(n) {
   if (n === 0) {
     return 1;
   }
-  return n * factorial(n - 1);
+  return n * recursiveFactorial(n - 1);
 }
-console.log(factorial(0)); // 1
-console.log(factorial(1)); // 1
-console.log(factorial(5)); // 120
+console.log(recursiveFactorial(0)); // 1
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(5)); // 120
 
 /*
   Big-O Notation:-
